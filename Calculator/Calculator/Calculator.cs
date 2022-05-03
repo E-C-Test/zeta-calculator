fa333848c6c23b0caed5953ed69a9ffeec7772ac
Buzo <b.bektic@eundc.at>
2022-05-03 11:30:37 +0200

    Buzos Änderung 01

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -5,5 +5,9 @@
         private int _existingProjects = 0;
 
         public void Init(int existingProjects) => _existingProjects = existingProjects;
+        public void Buzo(int myprojects)
+        {
+            _existingProjects *= myprojects;
+        }
     }
 }
