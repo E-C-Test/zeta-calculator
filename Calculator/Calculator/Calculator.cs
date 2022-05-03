cc50e2ba6e572a0be82a33dff3ab7ad7a5069f52
Severin Bauer <s.bauer@eundc.at>
2022-05-03 11:42:10 +0200

    Subtract projects

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -5,5 +5,10 @@
         private int _existingProjects = 0;
 
         public void Init(int existingProjects) => _existingProjects = existingProjects;
+
+        public void Sub(int otherProjects)
+        {
+            _existingProjects -= otherProjects;
+        }
     }
 }
