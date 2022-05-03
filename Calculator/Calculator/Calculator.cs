e5057dd7d1675288c5a3c75aee192d615087a419
Bertl1980 <h.zoechling@eundc.at>
2022-05-03 11:28:46 +0200

    Add project created

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -5,5 +5,10 @@
         private int _existingProjects = 0;
 
         public void Init(int existingProjects) => _existingProjects = existingProjects;
+
+        public void Addprojekt(int newProject)
+        {
+            _existingProjects += newProject;
+        }
     }
 }
